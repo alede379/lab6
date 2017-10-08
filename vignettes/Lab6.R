41ebce2ffc916db2e0fae03c2f4e1026bb96a3d7
Alessia De Biase <alede379@student.liu.se>
2017-10-08 19:14:32 +0200

    need to pull

--- vignettes/Lab6.R
+++ vignettes/Lab6.R
@@ -15,24 +15,3 @@ system.time(brute_force_knapsack(x = knapsack_objects[1:16,], W = 3500,parallel 
 ## ------------------------------------------------------------------------
 system.time(knapsack_dynamic(x = knapsack_objects[1:500,], W = 3500))
 
-## ------------------------------------------------------------------------
-#system.time(greedy_knapsack(x = knapsack_objects[1:1000000,], W = 3500))
-
-## ------------------------------------------------------------------------
-greedy_knapsack(x = knapsack_objects[1:8,], W = 3500)
-
-## ------------------------------------------------------------------------
-brute_force_knapsack(x = knapsack_objects[1:8,], W = 3500)
-
-## ------------------------------------------------------------------------
-knapsack_dynamic(x = knapsack_objects[1:8,], W = 3500)
-
-## ------------------------------------------------------------------------
-system.time(greedy_knapsack(x = knapsack_objects[1:8,], W = 3500))
-
-## ------------------------------------------------------------------------
-system.time(brute_force_knapsack(x = knapsack_objects[1:8,], W = 3500))
-
-## ------------------------------------------------------------------------
-system.time(knapsack_dynamic(x = knapsack_objects[1:8,], W = 3500))
-
